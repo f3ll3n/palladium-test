346b5313f3d849e9c76785bcaf6a273067984f5f
Данила Корж <danila.schults@yandex.ru>
2022-03-27 09:54:51 +0300

    ready head and main banner

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 jQuery(function(){
     //Определяем меню и кнопки
-    let headerMain = $('.header-main');
     let phoneBtn = $('.phone-icon');
     let addressBtn = $('.map-icon');
 
+    //Интерактивные элементы баннера
+    let bnrItemAnimate =  $('.bnr_item_wrap');
     
     //Кривенько убираем меню в зависимости от ивента..
     phoneBtn[0].addEventListener('mouseover', () =>{
@@ -23,4 +24,40 @@ jQuery(function(){
         $('.header-main').css({'opacity': '1'});
         console.log('ушёл');
     });
+    for(let i = 0; i < $('.bnr_item_wrap').length; i++){
+        $('.bnr_item_wrap')[i].addEventListener('mouseover', () =>{
+            $('.bnr-position').addClass('main-wrap');
+            $('.bnr-position').removeClass('bnr-bottom');
+            $('.bnr-bottom').addClass('hide')
+            if($('.bnr_item_wrap')[i] == $('.bnr_item_wrap')[0]){
+                $('.main-wrap').addClass('main-wrap1');
+            }
+            else if($('.bnr_item_wrap')[i] == $('.bnr_item_wrap')[1]){
+                $('.main-wrap').addClass('main-wrap2');
+            }
+            else if($('.bnr_item_wrap')[i] == $('.bnr_item_wrap')[2]){
+                $('.main-wrap').addClass('main-wrap3');
+            }
+            else if($('.bnr_item_wrap')[i] == $('.bnr_item_wrap')[3]){
+                $('.main-wrap').addClass('main-wrap4');
+            }
+            else{
+
+            }
+            
+         });
+         $('.bnr_item_wrap')[i].addEventListener('mouseleave', () =>{
+            if($('.bnr_item_wrap')[i] == $('.bnr_item_wrap')[i]){
+                $('.bnr-position').removeClass('main-wrap');
+                $('.bnr-position').removeClass(`main-wrap1`);
+                $('.bnr-position').removeClass(`main-wrap2`);
+                $('.bnr-position').removeClass(`main-wrap3`);
+                $('.bnr-position').removeClass(`main-wrap4`);
+                $('.bnr-bottom').removeClass('hide');
+                $('.bnr-bottom').css({'background-image': '("../img/default-bg.jpg")'})
+                
+            }
+         });
+    }
+    
 })
